4527ad0915b3e9c83689bfd5603793b7ac12d81b
Igor <shturmak.i.i@gmail.com>
2019-06-02 18:20:43 +0300

    feat: work with frames, correct displaying on delete, select

--- animation-player/index.js
+++ animation-player/index.js
@@ -4,25 +4,31 @@ const framesList = document.querySelector('.frames__list');
 
 // Add new frame on click 
 addFrame.addEventListener('click', () => {
+  const currentSelectFrame = document.querySelector('.frame-select');
+  currentSelectFrame.classList.remove('frame-select');
+
   let frame = document.createElement('li');
-  frame.className = 'frame';
+  frame.className = 'frame frame-select';
   let number = document.querySelectorAll('.frame-number').length;
   frame.innerHTML = `
     <div class="left-top-corner"><p class="frame-number">${number + 1}</p></div>
     <div class="delete-frame frame-hide-functions"><img src="assets/delete.svg" alt="Delete frame" class="frame-icons-functions"></div>
     <div class="copy-frame frame-hide-functions"><img src="assets/copy.svg" alt="Copy frame" class="frame-icons-functions"></div>
     <div class="move-frame frame-hide-functions"><img src="assets/move-dots.svg" alt="Move frame" class="frame-icons-functions"></div>`;
-
   framesList.appendChild(frame);
 });
 
 
-function updateNumFrames(num) {
+function updateNumFrames(prevFrame) {
   const framesList = document.querySelectorAll('.frame');
-  for (let i = num - 1; i < framesList.length; i++) {
-    let frameNumber = document.querySelectorAll('.frame-number');
-    frameNumber[i].innerText = i + 1;
+  const frameNumber = document.querySelectorAll('.frame-number');
+  let iter = prevFrame || 0;
+  if (iter !== 0) {
+    iter = +prevFrame.innerText;
   }
+  for (let i = iter; i < framesList.length; i++) {
+    frameNumber[i].innerText = i + 1;
+  } 
 }
 
 
@@ -31,18 +37,51 @@ function deleteFrame() {
 
   for (let i = 0; i < delFrame.length; i++) {
     delFrame[i].addEventListener('click', (event) => {
-      let frame = event.path[2];
+      let frame = event.currentTarget.offsetParent;
       // check if only one frame in list
-      if ((frame.parentNode !== null) && (framesList.childElementCount !== 1)) {
+      if ((frame !== null) && (framesList.childElementCount !== 1)) {
+        const prevElement = event.currentTarget.offsetParent.previousElementSibling;
         frame.parentNode.removeChild(frame);
-        updateNumFrames(+frame.innerText);
+        updateNumFrames(prevElement);
+
+        if (!framesList.children[0].classList.contains('frame-select')) {
+          selectFrame(prevElement)
+        }        
       } 
+    });
+  }
+}
+
+
+function selectFrame(prevElement) {
+  let frames = document.querySelectorAll('.frame');
+  
+  // if delete first element previous element equals null
+  if (prevElement) {
+    prevElement.classList.add('frame-select');
+  } else if (prevElement === null) {
+    framesList.firstElementChild.classList.add('frame-select');
+  }
+  
+  // add function on frame click to change selection
+  for (let i = 0; i < frames.length; i++) {
+    frames[i].addEventListener('click', (event) => {
+      const lastSelectFrame = document.querySelector('.frame-select');
+
+      const currentSelectFrame = event.currentTarget;
+      currentSelectFrame.classList.add('frame-select');
+
+      const numSelectFrames = document.querySelectorAll('.frame-select');
+      if (lastSelectFrame && numSelectFrames.length === 2) {
+        lastSelectFrame.classList.remove('frame-select');
+      }
     });
   }
 }
 
 function draw() {
   deleteFrame();
+  selectFrame();
 }
 
-setInterval(draw, 1);
+setInterval(draw, 10);
